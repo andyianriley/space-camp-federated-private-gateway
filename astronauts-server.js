90e729ecc9c0fe2f5d92e22ffd3c933c147c99a4
andy.riley <andy.riley@autotrader.co.uk>
2021-04-18 20:07:43 +0100

    add a custom gateway

--- astronauts-server.js
+++ astronauts-server.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 const { ApolloServer, gql } = require("apollo-server");
 const { buildFederatedSchema } = require("@apollo/federation");
 const { getAstronaut, getAstronautByRef, getAstronauts} = require("./services/astronauts")
@@ -39,3 +41,27 @@ const server = new ApolloServer({
 server.listen({ port }).then(({ url }) => {
   console.log(`Astronauts service ready at ${url}`);
 });
+
+typeDefs.toString = function () {
+	return this.loc.source.body;
+};
+
+// register schema
+(async () => {
+	try {
+		console.log('Registering schema', typeDefs.toString());
+
+		await fetch('http://localhost:6001/schema/push', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body:JSON.stringify({
+				name: 'astronauts_service', // service name
+				version: 'v1', //service version, like docker container hash. Use 'latest' for dev env
+				type_defs: typeDefs.toString(),
+			}),
+		});
+		console.info('Schema registered successfully!');
+	} catch (err) {
+		console.error(`Schema registration failed: ${err.message}`);
+	}
+})();
--- missions-server.js
+++ missions-server.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 const { ApolloServer, gql } = require("apollo-server");
 const { buildFederatedSchema } = require("@apollo/federation");
 const { getMissionByAstronaut, getCrew, getMissionById, getMissions} = require("./services/missions")
@@ -52,3 +54,27 @@ const server = new ApolloServer({
 server.listen({ port }).then(({ url }) => {
   console.log(`Missions service ready at ${url}`);
 });
+
+typeDefs.toString = function () {
+	return this.loc.source.body;
+};
+
+// register schema
+(async () => {
+	try {
+		console.log('Registering schema', typeDefs.toString());
+
+		await fetch('http://localhost:6001/schema/push', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body:JSON.stringify({
+				name: 'missions_service', // service name
+				version: 'v1', //service version, like docker container hash. Use 'latest' for dev env
+				type_defs: typeDefs.toString(),
+			}),
+		});
+		console.info('Schema registered successfully!');
+	} catch (err) {
+		console.error(`Schema registration failed: ${err.message}`);
+	}
+})();
